a78817519e9d3a34bb3b55e9b3171e138a6c65c2
Abdalah Ayman <abdhayman22@gmail.com>
2024-07-28 23:18:19 +0300

    init appbar and colors

---
+++ lib/core/widgets/bottom_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  final List<Map> items;
+  final Color backgroundColor;
+  final Color selectedColor;
+  final Color unSelectedColor;
+  final Function(int index) onChange;
+
+  const BottomBar(
+      {super.key,
+      required this.items,
+      required this.backgroundColor,
+      required this.selectedColor,
+      required this.unSelectedColor,
+      required this.onChange});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _currentIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+
+    widget.onChange(index);
+  }
+
+  List<BottomNavigationBarItem> _buildBottomNavigationItems() {
+    return widget.items.map((item) {
+      return BottomNavigationBarItem(
+        icon: item['icon'],
+        label: item['label'],
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: _buildBottomNavigationItems(),
+      onTap: _onItemTapped,
+      currentIndex: _currentIndex,
+      backgroundColor: widget.backgroundColor,
+      selectedItemColor: widget.selectedColor,
+      unselectedItemColor: widget.unSelectedColor,
+      type: BottomNavigationBarType.fixed, // Add this line
+    );
+  }
+}
--- lib/presentation/pages/main_view.dart
+++ lib/presentation/pages/main_view.dart
@@ -1,3 +1,5 @@
+import 'package:ai_care/core/constants/app_color.dart';
+import 'package:ai_care/core/widgets/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 class MainView extends StatefulWidget {
@@ -8,12 +10,64 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+  int _currentPageIndex = 0;
+
+  final List<Widget> _pages = [
+    const Center(child: Text('Home ')),
+    const Center(child: Text('chat ')),
+    const Center(child: Text('Comminity')),
+    const Center(child: Text('Devices')),
+  ];
+
+  void changePage(int index) {
+    setState(() {
+      _currentPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: AppColors.primary,
+        actions: [
+          Icon(
+            Icons.person,
+            size: 25,
+            color: Colors.white,
+          )
+        ],
       ),
+      bottomNavigationBar: BottomBar(
+          backgroundColor: AppColors.primary,
+          selectedColor: Colors.red,
+          unSelectedColor: Colors.blue,
+          onChange: changePage,
+          items: [
+            {
+              'label': 'Home',
+              'icon': Icon(
+                Icons.home,
+                size: 25,
+              ),
+            },
+            {
+              'label': 'chat',
+              'icon': Icon(
+                Icons.chat,
+                size: 25,
+              ),
+            },
+            {
+              'label': 'Comminity',
+              'icon': Icon(Icons.social_distance, size: 25),
+            },
+            {
+              'label': 'Devices',
+              'icon': Icon(Icons.phone_android, size: 25),
+            },
+          ]),
+      body: _pages[_currentPageIndex],
     );
   }
 }
